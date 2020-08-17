8d9c4095b0ef9b36df6e9339af291b06e0e073a7
Edy Segura <edysegura@gmail.com>
2020-08-17 14:50:47 -0300

    Try filter response entity

--- app/src/main/java/io/reflectoring/interceptor/ResponsePostProcessing.java
+++ app/src/main/java/io/reflectoring/interceptor/ResponsePostProcessing.java
@@ -1,22 +1,22 @@
 package io.reflectoring.interceptor;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import io.reflectoring.model.User;
-
 @ControllerAdvice
 public class ResponsePostProcessing implements ResponseBodyAdvice<Object> {
 
   @Override
   public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-    return true;
+    System.out.println("---> converterType: " + converterType.getName());
+    System.out.println("---> returnType: " + returnType.getParameterType().getTypeParameters());
+    return returnType.getParameterType().equals(ResponseEntity.class);
   }
 
   @Override
@@ -24,18 +24,7 @@ public class ResponsePostProcessing implements ResponseBodyAdvice<Object> {
       Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
       ServerHttpResponse response) {
 
-        response.setStatusCode(HttpStatus.BAD_REQUEST);
-        System.out.println("Passed here!!");
-
-        if(body instanceof User) {
-          User user = (User) body;
-          user.setPassword("****");
-          user.setEmail("AEP Spring");
-          System.out.println("Yes!!");
-        }
-        // System.out.println(body);
-
-        return body;
+    return body;
   }
 
 }

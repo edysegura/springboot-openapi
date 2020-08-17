9b0ecebdc6e61c8fe66c0f421ac1f342e78ed9f5
Edy Segura <edysegura@gmail.com>
2020-08-17 10:08:44 -0300

    Try kind of advice

--- app/src/main/java/io/reflectoring/interceptor/ResponsePostProcessing.java
+++ app/src/main/java/io/reflectoring/interceptor/ResponsePostProcessing.java
@@ -1,7 +1,6 @@
 package io.reflectoring.interceptor;
 
 import org.springframework.core.MethodParameter;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
@@ -9,33 +8,21 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import io.reflectoring.model.User;
-
 @ControllerAdvice
-public class ResponsePostProcessing implements ResponseBodyAdvice<Object> {
+public class ResponsePostProcessing implements ResponseBodyAdvice<String> {
 
   @Override
   public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
-    return true;
+    System.out.println("---> advice: " + returnType.getParameterType());
+    return returnType.getParameterType().equals(String.class);
   }
 
   @Override
-  public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType,
+  public String beforeBodyWrite(String body, MethodParameter returnType, MediaType selectedContentType,
       Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request,
       ServerHttpResponse response) {
-
-        response.setStatusCode(HttpStatus.BAD_REQUEST);
-        System.out.println("Passed here!!");
-
-        if(body instanceof User) {
-          User user = (User) body;
-          user.setPassword("****");
-          user.setEmail("AEP Spring");
-          System.out.println("Yes!!");
-        }
-        // System.out.println(body);
-
-        return body;
+    System.out.println("---> advice: " + returnType.getParameterType());
+    return body.toUpperCase();
   }
 
 }
